4c3eb9bb95e2afd23bade5a6c0866fdf47b566d5
YGL <2369015621@qq.com>
2017-02-28 21:30:55 +0800

    2017/2/28

--- app/src/main/java/cn/atd3/ygl/codemuseum/util/Utility.java
+++ app/src/main/java/cn/atd3/ygl/codemuseum/util/Utility.java
@@ -3,6 +3,8 @@ package cn.atd3.ygl.codemuseum.util;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Date;
+
 /**
  * Created by YGL on 2017/2/13.
  */
@@ -10,23 +12,9 @@ import org.json.JSONObject;
 public class Utility {
     //在这里解析JSON数据
 
-    //解析查询验证码返回JSON
-    // {"return":{"need":false}}
-    //{"return":{"need":true,"code":"\/verify.png?t=1487910827"}}
-    public static String getcheckcodeforJson(String jsonstr){
-        String need="";
-        try{
-            JSONObject jsonObject=new JSONObject(jsonstr);
-            jsonObject=jsonObject.getJSONObject("return");
-            need=jsonObject.getString("need");
-            if(need.equals("false")){
-                return need;
-            }else {
-                return jsonObject.getString("code");
-            }
-        }catch (JSONException e){
-            e.printStackTrace();
-        }
-        return null;
+    public static String beattoken="";
+
+    public static String urlAddT(String url){
+        return url+"&t="+String.valueOf(new Date().getTime());
     }
 }
